94a465abb42576f607561451c7b10b642d398562
panyi <yuyi.py@alibaba-inc.com>
2018-12-26 19:40:22 +0800

    show opengl es version

--- app/src/main/java/com/xinlan/vbovao/MainActivity.java
+++ app/src/main/java/com/xinlan/vbovao/MainActivity.java
@@ -1,7 +1,11 @@
 package com.xinlan.vbovao;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private MainSurfaceView mSurfaceView;
@@ -9,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo info = am.getDeviceConfigurationInfo();
+        //System.out.println("version = " + info.getGlEsVersion());
+        Toast.makeText(this, "version = " + info.getGlEsVersion(),Toast.LENGTH_LONG).show();
+        //String s = info.getGlEsVersion();
         mSurfaceView = new MainSurfaceView(this);
         setContentView(mSurfaceView);
     }
